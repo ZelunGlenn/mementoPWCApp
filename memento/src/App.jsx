93ffde03b67cd95454af71ae4f0bcddef848e15e
Zelun Zhang <121286574+ZelunGlenn@users.noreply.github.com>
2024-02-25 14:29:50 +1030

    setup project & updated assets and css

---
+++ memento/src/App.jsx
@@ -0,0 +1,13 @@
+// npx create-react-app my-app --template cra-template-pwa
+// for install
+
+// creting different folder for orientating.
+
+function App() {
+  return (
+    <div>
+    </div>
+  );
+}
+
+export default App;
